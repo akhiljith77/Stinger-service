d79e040b0a6b0f5bfee81575617c24dbe15e016b
Akhiljith <jithakhil811@gmail.com>
2025-01-11 17:41:15 +0530

    database connection done , registration success

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,17 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsNumber, IsString, MaxLength, MinLength } from "class-validator";
+
+export class CreateUserDto {
+
+    @IsString()
+    name:string
+
+    @IsEmail()
+    @IsNotEmpty()
+    email:string
+
+    @IsString()
+    @MinLength(8)
+    password:string
+
+
+}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn("uuid")
+  Id: string;
+  @Column()
+  name: string;
+
+  @Column({unique:true})
+  email: string;
+
+  @Column()
+  password: string;
+  @Column({nullable:true, unique:true})
+  phoneNumber: string;
+}
