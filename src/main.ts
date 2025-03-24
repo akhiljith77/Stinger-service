dfb53d047466c8724e0f0bf387ec0f2cbfc5079d
Hiran Raj <hirankvlr@gmail.com>
2025-03-24 17:13:24 +0530

    added the cors origin

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://192.168.1.4:5173'],
+    origin: ['http://localhost:5173', 'http://192.168.1.4:5173', 'http://stinger-ui.s3-website-ap-southeast-2.amazonaws.com'],
     methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
   });
